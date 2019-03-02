24b22eb816f688177cb2ceedba5f86de48b35871
Nicholas Rutherford <rutherfn@gmatc.matc.edu>
2019-03-01 19:09:11 -0600

    created the masthead component

--- components/src/components/Masthead/Masthead.js
+++ components/src/components/Masthead/Masthead.js
@@ -1,32 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Box } from 'rebass'
+import { Box, Link } from 'rebass'
 import styled from 'styled-components'
-import {Link} from 'rebass'
 
-const Outer  = styled(Box)`
-height: ${props => props.height}
+const Outer = styled(Box)`
+height: ${props => props.height};
 width: 100%;
 background-color: rebeccapurple;
 margin-bottom: 1.45rem;
 `
-export const Masthead = ({height,setTitle}) =>{
-    return(
+const StyledLink = styled(Link)`
+color: white;
+text-decoration: none;
+`
+
+export const Masthead = ({ height, siteTitle }) => {
+    return (
         <Outer height={height}>
-        <Link 
-        href="/"
-        stylle={{
-            color: `white`,
-            textDecoration:'none'
-        }}
-        >
-        {setTitle}
-        ></Link>
-        </Outer>
+        <StyledLink 
+        href="/">
+            {siteTitle}
+        </StyledLink>
+    </Outer>
     )
 }
 
-Masthead.PropTypes ={
+Masthead.propTypes = {
     height: PropTypes.string.isRequired,
-    setTitle: PropTypes.string
+    siteTitle: PropTypes.string
 }
