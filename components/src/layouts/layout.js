1a26b2b53a178edc2f816ef81b335f7811a88d15
Nicholas Rutherford <rutherfn@gmatc.matc.edu>
2019-03-07 18:11:31 -0600

    add DefaultLayout to Masthead

--- components/src/layouts/layout.js
+++ components/src/layouts/layout.js
@@ -1,50 +1,54 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-import "./layout.css"
-import { ThemeProvider } from 'styled-components'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+import './layout.css'
+import { ThemeProvider, ThemeConsumer } from 'styled-components'
 import { Gray as theme } from '../themes/Gray'
 import { Footer, Main } from '../components/Element'
-import { Masthead } from '../components/Masthead'
+import { Masthead, DefaultLayout } from '../components/Masthead'
 
 const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <div>
-        <Masthead siteTitle={data.site.siteMetadata.title} height="75px" />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <Main>{children}</Main>
-          <Footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </Footer>
+      `}
+      render={data => (
+        <div>
+          <Masthead height="85px">
+            <ThemeConsumer>
+              {theme => <DefaultLayout image={theme.images.mastheadImage} />}
+            </ThemeConsumer>
+          </Masthead>
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0
+            }}
+          >
+            <Main mt={85}>{children}</Main>
+            <Footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </Footer>
+          </div>
         </div>
-      </div>
-    )}
-  />
+      )}
+    />
   </ThemeProvider>
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
