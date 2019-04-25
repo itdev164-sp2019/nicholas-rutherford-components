5b65805f70caf8840f81eb7ff763656d7f7e38c9
Nicholas Rutherford <rutherfn@gmatc.matc.edu>
2019-04-25 09:24:01 -0500

    completed activity-9

--- components/src/pages/index.js
+++ components/src/pages/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
-import Layout from '../layouts/layout'
-import Image from '../gatsby-components/image'
-import SEO from '../gatsby-components/seo'
-import { Button, IconButton, Section } from '../components/Element'
-import { Search } from 'styled-icons/feather'
+import Layout from "../layouts/layout"
+import Image from "../gatsby-components/image"
+import SEO from "../gatsby-components/seo"
+import{Button, IconButton, Section} from "../components/Element"
+import {Search} from 'styled-icons/feather'
+import {ShareButtonList} from '../components/SocialShareButton'
 
 const IndexPage = () => (
   <Layout>
@@ -16,11 +17,19 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <ShareButtonList
+    addThisPubId="ra-5cc0b1d3cb9d02ab"
+    googleAnalyticsId="UA-763513-3"
+    contentTitle="Fake title"
+    contentImage="http://placekitten.com/g/300/700"
+    contentDescription=" Here is an article about science things, and maybe kittens."
+    />
     <Section flex justifyContent="space-evenly" m={2}>
       <Link to="/page-2/">Go to page 2</Link>
       <Button variant="primary">My Custom Button!</Button>
-      <IconButton icon={<Search />} />
+      <IconButton icon={<Search/>}/>
     </Section>
+  
   </Layout>
 )
 
